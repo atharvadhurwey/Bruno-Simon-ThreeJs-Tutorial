a03036cfd8bf4f58bf6bb3a30b8f52219290c3c6
Atharva Dhurwey <atharva.dhurwey.2000@gmail.com>
2023-06-20 15:04:24 +0530

    Basic Scene

---
+++ 01-basic-scene/script.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('canvas.webgl')
+
+//Scene
+const scene = new THREE.Scene()
+//blue cube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 'blue'})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+//sizes
+const sizes = {
+    width:800,
+    height:600
+}
+
+//camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+//renderer
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
